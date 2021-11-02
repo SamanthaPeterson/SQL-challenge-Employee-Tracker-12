6e165c5b07fd0e2acacc2d319ba87b84ec7b3a6f
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-11-02 15:02:42 -0600

    trying to resolve errors

--- db/schema.sql
+++ db/schema.sql
@@ -4,26 +4,28 @@ CREATE database employee_tracker_db;
 
 USE employee_tracker_db;
 
+
 CREATE TABLE department (
   id INTEGER NOT NULL auto_increment PRIMARY KEY,
-  name VARCHAR(30)
+  name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
   id INTEGER NOT NULL auto_increment PRIMARY KEY,
-  title VARCHAR(30),
-  salary DECIMAL,
+  title VARCHAR(30)NOT NULL,
+  salary DECIMAL NOT NULL,
   department_id INTEGER,
   FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
   id INTEGER NOT NULL auto_increment PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
   role_id INTEGER,
   manager_id INTEGER,
   FOREIGN KEY (role_id) REFERENCES role(id),
   FOREIGN KEY (manager_id) REFERENCES role(id)
 );
+
 
