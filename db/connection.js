cabb1b0d06a625763391c650f17c080aa07cbe29
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-11-05 16:33:23 -0600

    fixed connection now working

--- db/connection.js
+++ db/connection.js
@@ -3,13 +3,11 @@ require("dotenv").config();
 
 const db = mysql.createConnection(
 {
-      //port: 3306,
+      port: 3306,
       host: process.env.DB_HOST,
-      username: process.env.DB_USER,
+      user: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: 'employee_tracker_db'
-    },
-    console.log ('connected to the employee_tracker_db.')
-    )
+});
 
 module.exports = db;
