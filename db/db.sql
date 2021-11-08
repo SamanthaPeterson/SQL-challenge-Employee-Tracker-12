397c2d7901346f114c43e2d582877036c4005128
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-11-07 21:45:08 -0700

    update db sql

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,8 @@
 DROP DATABASE IF EXISTS db_employee_tracker;
 CREATE DATABASE db_employee_tracker;
 USE db_employee_tracker;
+
+
+-- DROP DATABASE IF EXISTS election;
+-- CREATE DATABASE election;
+-- USE election;
