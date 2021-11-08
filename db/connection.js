2933cafd5612e72f8bd29e58437c7edf9bd95543
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-11-08 09:13:24 -0700

    trying to trouble shoot error

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,12 @@
-import { createConnection } from 'mysql2';
+const mysql = require('mysql2');
 
-const db = createConnection({
+const db = mysql.createConnection({
   host: 'localhost',
   // Your MySQL username,
   user: 'root',
   // Your MySQL password
   password: 'DLP82418!s',
-  database: 'db_employee_tracker'
+  database: 'election'
 });
 
-export default db;
+module.exports = db;
--- db/schema.sql
+++ db/schema.sql
@@ -4,37 +4,24 @@ DROP TABLE IF EXISTS departments;
 DROP TABLE IF EXISTS employees;
 
 CREATE TABLE departments (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  description TEXT
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
 );
 
-CREATE TABLE managers (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  department_id INTEGER,
-  salary BOOLEAN NOT NULL,
-  CONSTRAINT fk_department
-    FOREIGN KEY (department_id)
-    REFERENCES departments(id)
-    ON DELETE SET NULL
+CREATE TABLE manager (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE employees (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  employee_id VARCHAR(50) NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE roles (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  employee_id INTEGER NOT NULL,
-  manager_id INTEGER NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT uc_employee UNIQUE (employee_id),
-  CONSTRAINT fk_employee FOREIGN KEY (employee_id) REFERENCES employees(id) ON DELETE CASCADE,
-  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES managers(id) ON DELETE CASCADE
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  manager_id INT,
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
