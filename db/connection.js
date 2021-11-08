0fea2d3b810769002f88ab1f7ddb97947e9aafe2
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-11-07 20:51:03 -0700

    restarting

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,12 @@
-const mysql = require("mysql2");
-require("dotenv").config();
+const mysql = require('mysql2');
 
-const db = mysql.createConnection(
-{
-      port: 3306,
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: 'employee_tracker_db'
+const db = mysql.createConnection({
+  host: 'localhost',
+  // Your MySQL username,
+  user: 'root',
+  // Your MySQL password
+  password: '',
+  database: 'election'
 });
 
 module.exports = db;
